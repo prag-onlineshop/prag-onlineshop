c1fb7c6c96f6da53ac7161c72dc715df48c53c4b
Sanny30 <sannymiraflor30@gmail.com>
2019-05-14 14:14:02 +0800

    add some schema

---
+++ database/migrations/2019_05_14_060336_create_cart_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('product_id');
+            $table->integer('cart_id');
+            $table->integer('quantity');
+            $table->integer('amount');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_products');
+    }
+}
