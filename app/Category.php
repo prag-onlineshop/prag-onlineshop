c1f1d6cd4c67e001502fbb10bc423c369f46fd7d
unknown <jlact94@gmail.com>
2019-05-22 14:57:13 +0800

    .

--- app/Category.php
+++ app/Category.php
@@ -20,13 +20,8 @@ class Category extends Model
 
     protected $guarded = [];
 
-<<<<<<< HEAD
-
-
-}
-=======
     public function products(){
         return $this->hasMany(Product::class);
     }
 }
->>>>>>> 6bb5100b930e78b3285096a7112468d17122e9ad
+git
