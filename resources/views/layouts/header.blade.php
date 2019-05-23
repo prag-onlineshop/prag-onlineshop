d6384ca5a0133997544568f5f5cd1e09206615f5
Sanny30 <sannymiraflor30@gmail.com>
2019-05-23 13:22:36 +0800

    Remove layouts for front end

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,20 +13,16 @@
                      <li><a href="/profile">Profile</a> </li>
                      <li><a href="#">WishList</a> </li>
                      <li><a href="/orders">Orders</a></li>
-                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
+                     <li><a href="#">{{ Auth::user()->name }}</a></li>
+                     <li>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+                        logout
                         </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                           <a class="dropdown-item" href="{{ route('logout') }}"
-                              onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">
-                           {{ __('Logout') }}
-                           </a>
-                           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                              @csrf
-                           </form>
-                        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                           @csrf
+                        </form>
                      </li>
                   @endguest
                </ul>
