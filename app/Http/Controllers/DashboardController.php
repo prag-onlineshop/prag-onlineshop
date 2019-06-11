facf96a3149a8ffc3aa1cfc11cf588f0906ebc19
jpaodelrosario5@gmail.com <jpaodelrosario5@gmail.com>
2019-06-11 12:35:42 +0800

    add more info in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,11 +15,14 @@ class DashboardController extends Controller
     	$orders = Carts::where('status', 'pending')->count();
     	$products = Product::whereDate('created_at', '=', date('Y-m-d'))->get()->count();
 
+
+    	$usersList = User::all();
+
     	$chart = new UserChart;
     	$chart->labels(['One', 'Two', 'Three']);
     	$chart->dataset('My dataset 1', 'line', [1, 2, 3]);
 
-    	return view('admin.dashboard.dashboard', compact('users', 'orders', 'chart', 'products'));
+    	return view('admin.dashboard.dashboard', compact('users', 'orders', 'chart', 'products', 'usersList'));
 
 
 
